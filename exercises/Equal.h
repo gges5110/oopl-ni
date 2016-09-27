fd0fb7bbfe89c307e47d38b60001ee4de0b1e4a5
yw7345 <gges5110@gmail.com>
2016-09-27 11:06:45 -0500

    Equal.h

---
+++ exercises/Equal.h
@@ -0,0 +1,9 @@
+template<typename InputIt1, typename InputIt2 >
+bool my_equal( InputIt1 first1, InputIt1 last1, InputIt2 first2 ) {
+    for (; first1 != last1; ++first1, ++first2) {
+        if (*first1 != *first2) {
+            return false;
+        }
+    }
+    return true;
+}
