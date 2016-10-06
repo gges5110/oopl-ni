e21feb5eaf549336a9234d3d68c86b40257e9816
gges5110 <gges5110@gmail.com>
2016-10-06 11:23:10 -0500

    another commit

--- exercises/Equal.h
+++ exercises/Equal.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // -------
 // Equal.h
 // -------
@@ -15,3 +16,14 @@ bool my_equal (II1 b, II1 e, II2 c) {
     return true;}
 
 #endif // Equal_h
+=======
+template<typename InputIt1, typename InputIt2 >
+bool my_equal( InputIt1 first1, InputIt1 last1, InputIt2 first2 ) {
+    for (; first1 != last1; ++first1, ++first2) {
+        if (*first1 != *first2) {
+            return false;
+        }
+    }
+    return true;
+}
+>>>>>>> Equal.h
